8d25bd7bd8c3a9811aca1a4d9108e49d752b6918
Musa Sayar <musasayar67@gmail.com>
2024-10-06 18:22:15 +0300

    first commit

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,37 +1,94 @@
-import { Tabs } from 'expo-router';
-import React from 'react';
+import { View, Text, Image } from "react-native";
+import React from "react";
+import { Tabs } from "expo-router";
+import { icons } from "@/constants";
 
-import { TabBarIcon } from '@/components/navigation/TabBarIcon';
-import { Colors } from '@/constants/Colors';
-import { useColorScheme } from '@/hooks/useColorScheme';
-
-export default function TabLayout() {
-  const colorScheme = useColorScheme();
-
-  return (
-    <Tabs
-      screenOptions={{
-        tabBarActiveTintColor: Colors[colorScheme ?? 'light'].tint,
-        headerShown: false,
-      }}>
-      <Tabs.Screen
-        name="index"
-        options={{
-          title: 'Home',
-          tabBarIcon: ({ color, focused }) => (
-            <TabBarIcon name={focused ? 'home' : 'home-outline'} color={color} />
-          ),
-        }}
-      />
-      <Tabs.Screen
-        name="explore"
-        options={{
-          title: 'Explore',
-          tabBarIcon: ({ color, focused }) => (
-            <TabBarIcon name={focused ? 'code-slash' : 'code-slash-outline'} color={color} />
-          ),
-        }}
-      />
-    </Tabs>
-  );
+type TabIconProps = {
+  icon:string,
+  color:string,
+  name:string,
+  focused:any
 }
+
+const TabIcon = ({ icon, color, name, focused }:TabIconProps) => {
+  return <View className="items-center justify-center gap-1">
+  <Image source={icon} resizeMode="contain" tintColor={color}
+  className="w-6 h-6"
+  />
+  <Text className={`${focused ? "font-psemibold":"font-pregular"} text-xs`}>
+  {name}
+  </Text>
+  </View>;
+};
+
+const TabsLayout = () => {
+  return (
+    <>
+      <Tabs screenOptions={{ tabBarShowLabel: false }}>
+        <Tabs.Screen
+          name="home"
+          options={{
+            title: "Home",
+            headerShown: false,
+            tabBarIcon: ({ color, focused }) => (
+              <TabIcon
+                icon={icons.home}
+                color={color}
+                focused={focused}
+                name={"Home"}
+              />
+            ),
+          }}
+        />
+        <Tabs.Screen
+          name="bookmark"
+          options={{
+            title: "Bookmark",
+            headerShown: false,
+            tabBarIcon: ({ color, focused }) => (
+              <TabIcon
+                icon={icons.bookmark}
+                color={color}
+                focused={focused}
+                name={"Bookmark"}
+              />
+            ),
+          }}
+        />
+        <Tabs.Screen
+          name="create"
+          options={{
+            title: "Create",
+            headerShown: false,
+            tabBarIcon: ({ color, focused }) => (
+              <TabIcon
+                icon={icons.plus}
+                color={color}
+                focused={focused}
+                name={"Create"}
+              />
+            ),
+          }}
+        />
+
+        <Tabs.Screen
+          name="profile"
+          options={{
+            title: "Profile",
+            headerShown: false,
+            tabBarIcon: ({ color, focused }) => (
+              <TabIcon
+                icon={icons.profile}
+                color={color}
+                focused={focused}
+                name={"Profile"}
+              />
+            ),
+          }}
+        />
+      </Tabs>
+    </>
+  );
+};
+
+export default TabsLayout;
---
+++ app/index.tsx
@@ -0,0 +1,15 @@
+import { StatusBar } from "expo-status-bar";
+import { Text, View } from "react-native";
+import { Link } from "expo-router";
+import React from "react";
+export default function Index() {
+  return (
+    <View className="flex-1 items-center justify-center">
+      <Text className="text-red-500 font-pblack">Hello my life </Text>
+      <StatusBar style="auto" />
+      <Link href="/home" style={{ color: "blue" }}>
+        Go to home
+      </Link>
+    </View>
+  );
+}
